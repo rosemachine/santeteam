7aaea3ad49362220f4723ae5f94622cbfb2b876d
noelnico <nicolas.noel@edu.ece.fr>
2018-03-24 17:37:13 +0100

    update sql

---
+++ patientbook (1).sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 24 Mars 2018 à 17:36
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `patientbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `ida` int(11) NOT NULL,
+  `idp` int(11) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `idc` int(11) NOT NULL,
+  `ida` int(11) NOT NULL,
+  `commentaire` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `couple_docart`
+--
+
+CREATE TABLE `couple_docart` (
+  `idd` int(11) NOT NULL,
+  `ida` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `couple_pd`
+--
+
+CREATE TABLE `couple_pd` (
+  `idp` int(11) NOT NULL,
+  `idd` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `data`
+--
+
+CREATE TABLE `data` (
+  `idata` int(11) NOT NULL,
+  `idp` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `bloodanalysis` int(11) NOT NULL,
+  `sugar` int(11) NOT NULL,
+  `temperature` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `data`
+--
+
+INSERT INTO `data` (`idata`, `idp`, `weight`, `bloodanalysis`, `sugar`, `temperature`) VALUES
+(1, 1, 7979, 0, 9879, 0),
+(2, 1, 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `idd` int(11) NOT NULL,
+  `sname` varchar(50) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `speciality` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `patient`
+--
+
+CREATE TABLE `patient` (
+  `idp` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `bloodtype` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`ida`);
+
+--
+-- Index pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`idc`);
+
+--
+-- Index pour la table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`idata`);
+
+--
+-- Index pour la table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`idd`);
+
+--
+-- Index pour la table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`idp`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `ida` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `idc` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `data`
+--
+ALTER TABLE `data`
+  MODIFY `idata` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `idd` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `idp` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
