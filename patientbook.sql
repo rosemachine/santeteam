79289ca17f402b91ffa27072d9a8d727537090a9
noelnico <nicolas.noel@edu.ece.fr>
2018-03-24 17:29:17 +0100

    Patientbook.sql update

--- patientbook.sql
+++ patientbook.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 17 Mars 2018 à 15:26
+-- Généré le :  Sam 24 Mars 2018 à 17:28
 -- Version du serveur :  10.1.19-MariaDB
 -- Version de PHP :  5.6.28
 
@@ -27,9 +27,32 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `article` (
-  `idp` int(11) NOT NULL,
   `ida` int(11) NOT NULL,
+  `idp` int(11) NOT NULL,
   `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `idc` int(11) NOT NULL,
+  `ida` int(11) NOT NULL,
+  `commentaire` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `couple_docart`
+--
+
+CREATE TABLE `couple_docart` (
+  `idd` int(11) NOT NULL,
+  `ida` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -50,12 +73,21 @@ CREATE TABLE `couple_pd` (
 --
 
 CREATE TABLE `data` (
+  `idata` int(11) NOT NULL,
   `idp` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
   `bloodanalysis` int(11) NOT NULL,
   `sugar` int(11) NOT NULL,
   `temperature` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `data`
+--
+
+INSERT INTO `data` (`idata`, `idp`, `weight`, `bloodanalysis`, `sugar`, `temperature`) VALUES
+(1, 1, 7979, 0, 9879, 0),
+(2, 1, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -102,19 +134,19 @@ CREATE TABLE `patient` (
 -- Index pour la table `article`
 --
 ALTER TABLE `article`
-  ADD PRIMARY KEY (`idp`);
+  ADD PRIMARY KEY (`ida`);
 
 --
--- Index pour la table `couple_pd`
+-- Index pour la table `comment`
 --
-ALTER TABLE `couple_pd`
-  ADD PRIMARY KEY (`idp`,`idd`);
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`idc`);
 
 --
 -- Index pour la table `data`
 --
 ALTER TABLE `data`
-  ADD PRIMARY KEY (`idp`);
+  ADD PRIMARY KEY (`idata`);
 
 --
 -- Index pour la table `doctor`
@@ -132,6 +164,21 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `ida` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `idc` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `data`
+--
+ALTER TABLE `data`
+  MODIFY `idata` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `doctor`
 --
